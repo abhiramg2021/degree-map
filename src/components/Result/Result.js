1a89fde8722cc662288d367994eff0cee8cc6199
abhiram <abhiramg2021@gmail.com>
2021-12-19 22:02:46 -0500

    verify function for pre req added

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -7,6 +7,8 @@ import { useState } from "react";
 import { FaInfoCircle, FaShareAlt, FaPlus } from "react-icons/fa";
 export const Result = ({ course, courseId }) => {
   const inputText = useSelector((state) => state.inputText);
+  const semesters = useSelector((state) => state.semesters);
+  const semesterCourses = useSelector((state) => state.semesterCourses);
   const dispatch = useDispatch();
   const size = "15px";
   const { addCourse } = bindActionCreators(actionCreators, dispatch);
@@ -19,41 +21,89 @@ export const Result = ({ course, courseId }) => {
   };
   const [showPrereqs, setShowPrereqs] = useState(false);
 
-
-  const recursiveReq = (prs,orLevel,andLevel) => {
+  const recursiveReq = (prs, orLevel, andLevel) => {
     let type = prs[0];
-    let level = andLevel === undefined ? 1 : andLevel
-    level = "level_" + level
+    let level = andLevel === undefined ? 1 : andLevel;
+    level = "level_" + level;
     if (Array.isArray(prs)) {
-
       prs = prs.slice(1);
       if (type === "or") {
-        return <div>{prs.map(p => recursiveReq(p,orLevel + 1, andLevel))}</div>
+        return (
+          <div>{prs.map((p) => recursiveReq(p, orLevel + 1, andLevel))}</div>
+        );
       } else if (type === "and") {
-        if (orLevel === 1 && andLevel === 0){
-          return prs.map(p => recursiveReq(p,orLevel, andLevel + 1)).reduce((prev, curr) => [prev, selectRender, curr])
+        if (orLevel === 1 && andLevel === 0) {
+          return prs
+            .map((p) => recursiveReq(p, orLevel, andLevel + 1))
+            .reduce((prev, curr) => [prev, selectRender, curr]);
         }
 
-        return <div>{recursiveReq(prs[0])}<ul>{prs.slice(1).map(p => recursiveReq(p,orLevel, andLevel + 1))}</ul></div>
+        return (
+          <div>
+            {recursiveReq(prs[0])}
+            <ul>
+              {prs.slice(1).map((p) => recursiveReq(p, orLevel, andLevel + 1))}
+            </ul>
+          </div>
+        );
       }
     } else {
-      let len = prs['id'].length * 12
-      level = "req " + level
-      return <li className = {level} style = {{width: `${len}px`}}>{prs['id']}</li>
+      let len = prs["id"].length * 12;
+      level = "req " + level;
+      return (
+        <li className={level} style={{ width: `${len}px` }}>
+          {prs["id"]}
+        </li>
+      );
     }
   };
 
-  const selectRender = <div className = "select">Select one of the following</div>
+  const verifyReq = (prqs) => {
+
+    let cond = false
+    if (Array.isArray(prqs)){
+      let type = prqs[0]
+      prqs = prqs.slice(1) 
+      if (type === "or"){
+        prqs.forEach(p =>{
+          cond = verifyReq(p) ? true: cond
+        })
+        return cond
+      } else if (type === "and"){
+        prqs.forEach(p =>{
+          cond = verifyReq(p) ? true : false
+        })
+        return cond
+      }
+    } else{
+      return searchReq(prqs.id)
+    }
+    
+  };
+
+  const searchReq = (cr) => {
+    let currentSem = inputText.semId + 1;
+    let cond = 0;
+    semesters.slice(0, currentSem).every((s) => {
+         s.courseIds.every((courseId) => {
+          cond = cr === semesterCourses[courseId].code ? cond + 1 : cond;
+        })
+    });
+    return cond === 0 ? false: true
+  };
+  const selectRender = (
+    <div className="select">Select one of the following</div>
+  );
   const preReqRender = (prereqs) => {
     if (showPrereqs === true) {
       if (prereqs.length === 0) {
         return <div className="Prereqs none">There are no prerequisites!</div>;
       }
-
+      console.log("End result: " + verifyReq(prereqs));
       return (
         <div className="Prereqs multiple">
           {selectRender}
-          {recursiveReq(prereqs,0, 0)}
+          {recursiveReq(prereqs, 0, 0)}
         </div>
       );
     }
